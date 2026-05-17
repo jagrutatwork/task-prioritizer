06f3ed3a5b9cdc28f3001b9cd9eca9cc1edc6853
jagrutatwork <jagrutatwork@gmail.com>
2026-05-17 15:24:37 +0530

    Update TicketController.java

--- src/main/java/com/jagrut/taskprioritizer/controller/TicketController.java
+++ src/main/java/com/jagrut/taskprioritizer/controller/TicketController.java
@@ -1,33 +1,30 @@
 package com.jagrut.taskprioritizer.controller;
 
 import com.jagrut.taskprioritizer.entity.Ticket;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController //Controller + Response Body
-//Meaning:
-//handles HTTP requests
-//returns JSON automatically
+@RestController
 @RequestMapping("/tickets")
 public class TicketController {
 
-    @PostMapping()
-    public boolean addTicket (@RequestBody Ticket ticket)
-    {
+    @PostMapping
+    public ResponseEntity<String> addTicket(@RequestBody Ticket ticket) {
 
-        return true;
+        return ResponseEntity.ok("Ticket added successfully");
     }
 
-    @PutMapping
-    public boolean modifyTicket (@RequestBody Ticket ticket)
-    {
-        return true;
+    @PutMapping("/{id}")
+    public ResponseEntity<String> modifyTicket(
+            @PathVariable Long id,
+            @RequestBody Ticket ticket) {
+
+        return ResponseEntity.ok("Ticket updated successfully");
     }
 
     @DeleteMapping("/{id}")
-    public boolean deleteTicket (@PathVariable Long ticketId){
-        return true;
+    public ResponseEntity<String> deleteTicket(@PathVariable Long id) {
+
+        return ResponseEntity.ok("Ticket deleted successfully");
     }
-
-
 }
